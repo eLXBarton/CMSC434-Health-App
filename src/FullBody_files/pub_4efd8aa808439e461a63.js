0dbeeba8932a344b29b03afe3ac8f4b6291fc2c8
eLXBarton <lxbarton@umd.edu>
2022-10-15 16:58:38 -0400

    Removed the html

--- src/FullBody_files/pub_4efd8aa808439e461a63.js
+++
@@ -1,8 +0,0 @@
-// Last edit: Thu, 29 Sep 22 20:09:16 -0400
-_rfsn_tracker.load_settings({
-	version : 2.0,
-	base_url : 'https://setforset.refersion.com/',
-	xdomains: [],
-	verbose: false,
-	is_loaded: true
-})
