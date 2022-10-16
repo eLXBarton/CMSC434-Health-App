9183bde9b585dfb88003b210a92dcb75db3f75e6
eLXBarton <lxbarton@umd.edu>
2022-10-16 14:25:54 -0400

    Made final page, just now have to connect everything

--- src/WorkoutPage.java
+++ src/WorkoutPage.java
@@ -7,25 +7,31 @@ import java.net.URL;
 
 public class WorkoutPage extends JFrame {
     JFrame frame; //this needs to be the container
-    JPanel panelWork, panelLog, panelPlan, panelHome; //these need to go in the frame
+    JPanel panelWork, panelLog, panelHist, panelHome; //these need to go in the frame
     JButton settings, back, log, plan, workout;
     JTextArea text1, text2, text3;
     java.net.URL web_page;
     JScrollPane scroller;
+    DefaultListModel<String> real;
+    JList<String> list;
 
     public WorkoutPage(){
+        
         frame = new JFrame();
         log = new JButton("History");
         plan = new JButton("Plans");
         workout = new JButton("Log Workout");
         settings = new JButton("Settings");
         back = new JButton("Back");
+        real = new DefaultListModel<String>();
+        list = new JList<String>(real);
 
         startPage();
-        historyPage();
+        logPage();
         planPage();
+        historyPage();
 
-        frame.add(panelWork);
+        frame.add(panelHist);
         
         /*TODO: add action listeners for plans, log, and workout
         *   make the pages for those buttons
@@ -37,6 +43,18 @@ public class WorkoutPage extends JFrame {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+    }
+
+    void historyPage(){
+        JScrollPane frame = new JScrollPane();
+        JPanel temp = new JPanel();
+        panelHist = new JPanel();
+        temp.add(list);
+        frame.setViewportView(temp);
+        JButton back = new JButton("Back");
+        panelHist.setLayout(new GridLayout(2,2));
+        panelHist.add(frame);
+        panelHist.add(back);
     }
 
     void startPage(){
@@ -70,7 +88,7 @@ public class WorkoutPage extends JFrame {
 
 
     void planPage(){
-        JButton full_body = new JButton("Full Body (3 Days)"), four = new JButton("Four Days"), cardio = new JButton("Cardio"), cross_fit = new JButton("Cross Fit");
+        JButton back = new JButton("Back"), full_body = new JButton("Full Body (3 Days)"), four = new JButton("Four Days"), cardio = new JButton("Cardio"), cross_fit = new JButton("Cross Fit");
         panelWork = new JPanel(new GridBagLayout());
         GridBagConstraints con = new GridBagConstraints();
         panelWork.setSize(frame.getSize());
@@ -109,11 +127,9 @@ public class WorkoutPage extends JFrame {
         panelWork.add(cross_fit, con);
     }
 
-    void historyPage(){
+    void logPage(){
         JButton submit = new JButton("Submit");
         JTextField entry = new JTextField();
-        DefaultListModel<String> real = new DefaultListModel<String>();
-        JList<String> list = new JList<String>(real);
         panelLog = new JPanel(new GridBagLayout());
         GridBagConstraints con = new GridBagConstraints();
         panelLog.setSize(frame.getSize());
