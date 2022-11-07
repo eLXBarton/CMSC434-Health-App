85dca133bd2c835b3d68b66aababae57b8386aca
PraiseTheLord0202 <78345568+PraiseTheLord0202@users.noreply.github.com>
2022-11-06 19:06:14 -0500

    Update JournalPage.java
    Fixed Journal Page Bugs

--- src/JournalPage/JournalPackage/JournalPage.java
+++ src/JournalPage/JournalPackage/JournalPage.java
@@ -17,18 +17,30 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
+import HomePage.HomePage;
+import HomePage.Profile;
+
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JLabel;
-import java.util.Scanner;
+
+
+
+import java.net.URL;
+
+
+import java.awt.Desktop;
+
+
+import javax.swing.JTextField;
 
 public class JournalPage extends JFrame {
 
 	private JPanel contentPane;
-	private AllEntries lst;
+	
 	private int count = 1;
     
     private JButton btnNewButton_3;
@@ -38,11 +50,16 @@ public class JournalPage extends JFrame {
 	String day = "";
 	JDialog d;
 	JButton[] button = new JButton[49];
+	JScrollPane scrollPane;
+	JPanel panel;
+	private JTextField txtJournalPage;
 
 	/**
 	 * Launch the application.
+	 * 
+	 * Testing purposes
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -59,7 +76,7 @@ public class JournalPage extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public JournalPage() {
+	public JournalPage(JFrame prev) {
 		setTitle("Journal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 375, 500);
@@ -69,7 +86,6 @@ public class JournalPage extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
 		JPanel calendarPanel = new JPanel();
 		calendarPanel.setBounds(95, 83, 266, 150);
 		contentPane.add(calendarPanel);
@@ -78,8 +94,7 @@ public class JournalPage extends JFrame {
 		d.setModal(true);
 		String[] header = { "S", "M", "T", "W", "TH", "F", "SA" };
 		JPanel p1 = new JPanel(new GridLayout(7, 7));
-		//p1.setPreferredSize(new Dimension(169, 170));
-		p1.setBounds(95,84,266,194);
+		p1.setBounds(95, 84, 266, 194);
 
 
 		for (int x = 0; x < button.length; x++) {
@@ -141,56 +156,64 @@ public class JournalPage extends JFrame {
 		contentPane.add(p1);
 		contentPane.add(p2);
 		
-		JTextArea txtrJournalPage = new JTextArea();
-		txtrJournalPage.setText("Journal Page");
-		txtrJournalPage.setBounds(95, 10, 117, 35);
-		contentPane.add(txtrJournalPage);
-		
-		JButton btnNewButton = new JButton("Home Page");
+		JButton btnNewButton = new JButton("Home \nPage");
 		btnNewButton.setBounds(0, 10, 95, 35);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				prev.setVisible(true);
+				
+			}
+		});
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Profile");
-		btnNewButton_1.setBounds(266, 12, 85, 21);
+		btnNewButton_1.setBounds(266, 12, 85, 33);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				Profile pPage = new Profile(prev);
+				pPage.frame.setVisible(true);
+				
+			}
+		});
 		contentPane.add(btnNewButton_1);
 		
 		JTextArea txtrCalendarChecklist = new JTextArea();
-		txtrCalendarChecklist.setText("Calendar Checklist");
 		txtrCalendarChecklist.setBounds(159, 55, 158, 22);
+		txtrCalendarChecklist.setText("Calendar Checklist");
+		txtrCalendarChecklist.setEditable(false);
 		contentPane.add(txtrCalendarChecklist);
 		
 		JTextArea txtrUpdateTheCalendar = new JTextArea();
-		txtrUpdateTheCalendar.setText("Update the calendar with \\n days you've worked out");
-		txtrUpdateTheCalendar.setBounds(10, 153, 75, 80);
+		txtrUpdateTheCalendar.setBounds(10, 153, 75, 95);
+		txtrUpdateTheCalendar.setText("Use the \ncalendar \nto track \ndays worked \nout");
+		txtrUpdateTheCalendar.setEditable(false);
 		contentPane.add(txtrUpdateTheCalendar);
 		
-		JButton btnNewButton_2 = new JButton("Advice Page");
+		JButton btnNewButton_2 = new JButton("Advice");
+		btnNewButton_2.setBounds(10, 83, 75, 46);
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				OpenAdviceTab();
+				
 			}
 		});
-		btnNewButton_2.setBounds(10, 83, 75, 46);
 		contentPane.add(btnNewButton_2);
 		
 		btnNewButton_3 = new JButton("Add Journal Entry");
+		btnNewButton_3.setBounds(105, 313, 227, 35);
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_3.setBounds(10, 312, 341, 57);
-		contentPane.add(btnNewButton_3);
 		
 		
+		scrollPane = new JScrollPane(scrollPane, 
+	    		   JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setBounds(24, 358, 308, 95);
+		panel = new JPanel();
 		
-		lst = new AllEntries();
-		
-		JScrollPane scroll = new JScrollPane (lst, 
-	    		   JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-	    contentPane.add(scroll);
-		
-		
-		lst.setBounds(20, 385, 331, 68);
-		contentPane.add(lst);
 		
 		btnNewButton_3.addMouseListener(new MouseAdapter() {
 			@Override
@@ -201,40 +224,58 @@ public class JournalPage extends JFrame {
 				
 			    
 				JournalEntry newEntry = new JournalEntry("Add Note", "Add Desc");
+				newEntry.setText(new Integer(count).toString());
+				newEntry.setMargin(new Insets(0,0,0,0));
+				newEntry.setFont(new Font("Nunito", Font.PLAIN, 12));
 				
 				
 				
 				
 				
 				newEntry.setBounds(40, 365 + count*30, 200, 30);
+				if (count % 3 == 0) {
+					newEntry.setBackground(Color.GREEN);
+					newEntry.setOpaque(true);
+				} else if (count % 3 == 1) {
+					newEntry.setBackground(Color.CYAN);
+					newEntry.setOpaque(true);
+				} else {
+					newEntry.setBackground(Color.GRAY);
+					newEntry.setOpaque(true);
+				}
 				count++;
-				lst.add(newEntry);
-				contentPane.add(newEntry);
+				panel.add(newEntry);
+				scrollPane.setViewportView(panel);
+				scrollPane.setVisible(true);
+				newEntry.setVisible(true);
+				revalidate();
 
 
-
-				/*newEntry.getDone().addMouseListener(new MouseAdapter() {
-
-					public void mousePressed(MouseEvent e)
-					{
-
-						newEntry.changeState();
-
-						revalidate();
-
-					}
-				}); */
 			}
 
 		});
-		//d.add(p1, BorderLayout.CENTER);
-        // d.add(p2, BorderLayout.SOUTH);
+		
+		contentPane.add(btnNewButton_3);
+		contentPane.add(scrollPane);
+		
+		txtJournalPage = new JTextField();
+		txtJournalPage.setText("Journal Page");
+		txtJournalPage.setBounds(120, 10, 111, 35);
+		txtJournalPage.setEditable(false);
+		txtJournalPage.setHorizontalAlignment(JTextField.CENTER);
+		contentPane.add(txtJournalPage);
+		txtJournalPage.setColumns(10);
+		
+		
+	
+		
+		
+		
         d.pack();
         d.setLocationRelativeTo(this);
         displayDate();
         p1.setVisible(true);
         p2.setVisible(true);
-        d.setVisible(true);
         
 
 
@@ -260,7 +301,7 @@ public class JournalPage extends JFrame {
 			button[x].setText("" + day);
 		    button[x].setMargin(new Insets(0,0,0,0));
 		}
-		//l.setText(sdf.format(cal.getTime()));
+		l.setText(sdf.format(cal.getTime()));
 		//d.setTitle("Date Picker");
 	}
 	
@@ -274,4 +315,14 @@ public class JournalPage extends JFrame {
          return sdf.format(cal.getTime());
  }
 	 
+	 private void OpenAdviceTab() {
+			String urlString = "https://www.mayoclinic.org/healthy-lifestyle/fitness/in-depth/fitness/art-20048269";
+		    try {
+		        Desktop.getDesktop().browse(new URL(urlString).toURI());
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+		}
 }
+
+
